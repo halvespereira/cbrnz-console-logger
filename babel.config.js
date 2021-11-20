3e32a62b592ece325b4452c929892b2b77124d5d
Henrique Pereira <henrique.pereira@terazo.com>
2021-11-20 12:33:27 -0700

    updated exporting

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-    "@babel/plugin-proposal-export-default-from",
-  ],
+  presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"],
 };
--- src/index.ts
+++ src/index.ts
@@ -66,4 +66,4 @@ function ConsoleLogger() {
   };
 }
 
-export default ConsoleLogger;
+module.exports = ConsoleLogger;
